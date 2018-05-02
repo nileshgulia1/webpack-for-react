1f89c9324708199928216533bf7b166de19be2eb
nileshgulia1 <nileshgulia@gmail.com>
2018-05-03 03:10:41 +0530

    added soome small configs ,, yeah very small :)

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  // Webpack configuration goes here
+};
